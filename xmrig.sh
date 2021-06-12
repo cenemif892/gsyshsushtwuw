77b939f1a776f118176069ab74014628a9260642
cenemif892 <85499919+cenemif892@users.noreply.github.com>
2021-06-12 19:12:44 +0000

    Create xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -2,5 +2,5 @@
 sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v5.5.1/xmrig-5.5.1-xenial-x64.tar.gz  
 tar -zxf xmrig-5.5.1-xenial-x64.tar.gz
-./xmrig-5.5.1
+cd/xmrig-5.5.1
 ./xmrig -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u cenemif891@gmail.com -p x --cpu4
