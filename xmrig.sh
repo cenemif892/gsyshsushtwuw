0c1a3013f61a39d27eeae5d3dfe68b0a98ec0ec4
cenemif892 <85499919+cenemif892@users.noreply.github.com>
2021-06-13 08:06:52 +0000

    Create xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,4 +1,4 @@
 #monero-coin
-sudo apt update
-wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && /xmrig-5.11.3 
-./xmrig -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u cenemif891@gmail.com -p x --cpu4
+!sudo apt update
+!wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && /xmrig-5.11.3 
+!./xmrig -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u cenemif891@gmail.com -p x --cpu4
